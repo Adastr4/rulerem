f302043e478d04ebe7da97aa30ab29bd48b3675b
MepAdm <MepAdm@weka.433wsiokmr0ubcqkgske1rwjmd.ax.internal.cloudapp.net>
2021-08-24 10:48:28 +0000

    nuova relase

---
+++ cart.test/src/main/java/cart/test/constraints/cs2/CLFINI_Rule1.java
@@ -0,0 +1,46 @@
+package cart.test.constraints.cs2;
+
+import java.util.List;
+
+import com.deliveredtechnologies.rulebook.RuleState;
+import com.deliveredtechnologies.rulebook.annotation.Given;
+import com.deliveredtechnologies.rulebook.annotation.Result;
+import com.deliveredtechnologies.rulebook.annotation.Rule;
+import com.deliveredtechnologies.rulebook.annotation.Then;
+import com.deliveredtechnologies.rulebook.annotation.When;
+
+import cart.test.CaratteristicaBean;
+import cart.test.ReadDB;
+
+@Rule(order = 1) // order specifies the order the rule should execute in; if not specified, any
+					// order may be used
+public class CLFINI_Rule1 {
+	private static final RuleState BREAK = null;
+
+	@Given
+	private List<CaratteristicaBean> caratteristiche; // Annotated Lists get injected with all Facts of the declared
+														// generic type
+
+	@Result
+	private boolean validate;
+
+	@When
+	public boolean when() {
+		System.out.println("executing rule 1 no package");
+		return getClasse().equalsIgnoreCase("EDT") && 
+				( caratteristiche.stream().anyMatch(caratteristica -> caratteristica.getCLFINI().equalsIgnoreCase("f")) ||
+						caratteristiche.stream().anyMatch(caratteristica -> caratteristica.getCLFINI().equalsIgnoreCase("n")) );		
+	}
+
+	@Then
+	public RuleState then() {
+		System.out.println("executed rule 1 no package");
+		validate = Boolean.TRUE;
+		return RuleState.BREAK;
+	}
+	private String getClasse() {
+		String classe = ReadDB.readClasse();
+		return classe;
+	}
+
+}
