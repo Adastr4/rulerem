0a4e21ae8625e10f4e01616a6b1bd6a5459f7e17
FilippoBenvenuti <filippo.benvenuti@profilglass.it>
2021-09-17 17:12:19 +0200

    Added some Attribute for BAV configurator

---
+++ cart.test/src/main/java/it/profilglass/constraint/bav/DLLATOLP/RuleDLLATOLP_1.java
@@ -0,0 +1,35 @@
+package it.profilglass.constraint.bav.DLLATOLP;
+
+import java.util.List;
+
+import com.deliveredtechnologies.rulebook.RuleState;
+import com.deliveredtechnologies.rulebook.annotation.Given;
+import com.deliveredtechnologies.rulebook.annotation.Result;
+import com.deliveredtechnologies.rulebook.annotation.Rule;
+import com.deliveredtechnologies.rulebook.annotation.Then;
+import com.deliveredtechnologies.rulebook.annotation.When;
+
+import test.test.CaratteristicaBean;
+
+@Rule(order = 1, name = "ruleDLLATOLP1")
+
+public class RuleDLLATOLP_1 {
+	@Given 
+	private CaratteristicaBean caratteristica; //Annotated Lists get injected with all Facts of the declared generic type
+
+	@Result
+	private String result;
+	
+	@When
+	public boolean when()
+	{
+		return caratteristica.getSLTOLLA().equalsIgnoreCase("P"); //Da implementare quando diverranno effettive le nuove regole da applicare
+	}
+	
+	@Then
+	public RuleState then()
+	{
+		result = "0";
+		return RuleState.BREAK;
+	}
+}
