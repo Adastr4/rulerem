3157c6bb07a127a1575aeb7dbba23e689916a25a
FilippoBenvenuti <filippo.benvenuti@profilglass.it>
2021-09-20 15:56:19 +0200

    Added some rules

---
+++ cart.test/src/main/java/it/profilglass/constraint/bav/CLRIVE/RuleCLRIVE_1.java
@@ -0,0 +1,31 @@
+package it.profilglass.constraint.bav.CLRIVE;
+
+import com.deliveredtechnologies.rulebook.RuleState;
+import com.deliveredtechnologies.rulebook.annotation.Given;
+import com.deliveredtechnologies.rulebook.annotation.Result;
+import com.deliveredtechnologies.rulebook.annotation.Then;
+import com.deliveredtechnologies.rulebook.annotation.When;
+
+import test.test.CaratteristicaBean;
+
+public class RuleCLRIVE_1 {
+	@Given 
+	private CaratteristicaBean caratteristica; //Annotated Lists get injected with all Facts of the declared generic type
+
+	@Result
+	private String result = "A";
+	
+	@When
+	public boolean when()
+	{
+		return caratteristica.getCLFINI().equalsIgnoreCase("K") || caratteristica.getCLFINI().equalsIgnoreCase("Q") || caratteristica.getCLFINI().equalsIgnoreCase("W");
+	}
+	
+	@Then
+	public RuleState then()
+	{
+		result = "B";
+		return RuleState.BREAK;
+	}
+
+}
---
+++ cart.test/src/main/java/it/profilglass/constraint/bav/val/CLRIVE/RuleCLRIVE_1.java
@@ -0,0 +1,36 @@
+package it.profilglass.constraint.bav.val.CLRIVE;
+
+import java.util.List;
+
+import com.deliveredtechnologies.rulebook.RuleState;
+import com.deliveredtechnologies.rulebook.annotation.Given;
+import com.deliveredtechnologies.rulebook.annotation.Result;
+import com.deliveredtechnologies.rulebook.annotation.Rule;
+import com.deliveredtechnologies.rulebook.annotation.Then;
+import com.deliveredtechnologies.rulebook.annotation.When;
+
+import test.test.CaratteristicaBean;
+
+@Rule(order = 1, name = "ruleCLRIVE_1")
+
+public class RuleCLRIVE_1 {
+	@Given 
+	private List<CaratteristicaBean> caratteristiche; //Annotated Lists get injected with all Facts of the declared generic type
+
+	@Result
+	private boolean result;
+	
+	@When
+	public boolean when()
+	{
+		return true; //Da implementare quando diverranno effettive le nuove regole da applicare
+	}
+	
+	@Then
+	public RuleState then()
+	{
+		result = Boolean.TRUE;
+		return RuleState.BREAK;
+	}
+
+}
