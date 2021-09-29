05966a03cf5f522b2051d534737db194ad9841ce
nodejs <assistenza@adastrastartup.it>
2021-09-29 12:29:08 +0200

    sistemato dipendenze

--- cart.test/src/test/java/cart/test/CicloTest.java
+++ cart.test/src/test/java/cart/test/CicloTest.java
@@ -10,27 +10,31 @@ import com.deliveredtechnologies.rulebook.FactMap;
 import com.deliveredtechnologies.rulebook.NameValueReferableMap;
 import com.deliveredtechnologies.rulebook.model.runner.RuleBookRunner;
 
+/**
+ * per la generazione del ciclo A partire da una serie di caratteristiche viene
+ * restituito l'elenco delle macchine che possono lavorare il materiale
+ * descritto dalle cartteristiche
+ * 
+ * @author nodejs
+ * 
+ */
 class CicloTest {
-
-
 
 	@Test
 	void machinaTest() {
 		System.out.println("AAB");
 		RuleBookRunner ruleBook = new RuleBookRunner("pippo.test");
-		CaratteristicaBean applicant1 = new CaratteristicaBean(
-				new BigDecimal(650), "", "",  "", "", "", "");
+		CaratteristicaBean applicant1 = new CaratteristicaBean(new BigDecimal(650), "", "", "", "", "", "");
 		NameValueReferableMap<CaratteristicaBean> facts = new FactMap<>();
 		facts.put(new Fact<>(applicant1));
 		ruleBook.setDefaultResult(new ArrayList<Macchina>());
 		ruleBook.run(facts);
-		//Result<ArrayList> res = ruleBook.getResult().get();
-		//System.out.println("Macchina " + res.getValue());
-		ruleBook.getResult().ifPresent(result ->
-		{
-			ArrayList res = (ArrayList)result.getValue();
+		// Result<ArrayList> res = ruleBook.getResult().get();
+		// System.out.println("Macchina " + res.getValue());
+		ruleBook.getResult().ifPresent(result -> {
+			ArrayList res = (ArrayList) result.getValue();
 			System.out.println(res.get(0));
-			//assertTrue(test==(Boolean) result.getValue());
+			// assertTrue(test==(Boolean) result.getValue());
 		});
 	}
 }
